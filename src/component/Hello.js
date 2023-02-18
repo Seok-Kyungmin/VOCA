03b556741f5563a912b6b32600d6491200b89526
kyungminSeok <sukm386@naver.com>
2023-02-18 23:00:19 +0900

    'commit'

--- src/component/Hello.js
+++ src/component/Hello.js
@@ -1,10 +1,16 @@
-import World from "./World";
+import styles from "./Hello.module.css"
 
 export default function Hello() {
   return (
       <div>
-        <h1>Hello</h1>
-        <World />
+        <h1 style={{
+            color : '#f00',
+            borderRight : '2px solid #000',
+            marginBottom : '30px',
+            opacity : 0.5,
+          }}>
+            Hello</h1>
+        <div className={styles.box}>Hello</div>
       </div>
     );
-};
+}
